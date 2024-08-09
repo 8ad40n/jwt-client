210acd23801933e5a7c30710d72a7fa85281826d
8ad40n <joy184110@gmail.com>
2024-08-10 02:01:42 +0600

    fetch done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import React, { useContext } from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 
 export default function Home() {
   const {user} = useContext(AuthContext);
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/users")
+    .then(res=>{
+      console.log(res.data);
+      setUsers(res.data);
+    })
+  },[])
+
   return (
     <div>
       {
         user?
         <>
           <h1 className='flex justify-center'>Email: {user.email}</h1>
+          <h1>All users:</h1>
+          <div>
+            {
+              users.map(u=><h1 key={u._id}>{u.email}</h1>)
+            }
+          </div>
         </>:
         <>
           <h1 className='flex justify-center'>Please, Login First!</h1>
